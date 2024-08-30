ab93221b2866c03435f64ac623c5ddbf821d790b
Kunal <kunaldhadwaledneed@gmail.com>
2024-08-30 14:11:57 +0530

    naster

--- frontend/dist/assets/Login-Page-ZOWB8CCl.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CFHMJ-zO.js";import{A as r}from"./Auth-B41HuQT0.js";const i=()=>o.jsxs("div",{children:[o.jsx("h1",{children:"Login"}),o.jsx(r,{type:"login"})]});export{i as default};
